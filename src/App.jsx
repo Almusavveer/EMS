e7192f12eedb43be6db0486d2552eb44990a8780
almusavveer <67317335+Almusavveer@users.noreply.github.com>
2024-11-04 22:54:09 +0530

    Wrapping App in AuthContext and TaskContext is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+
 import './App.css'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-// import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-// import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import Login from './components/Auth/Login'
 
 const App = () => {
   return (
     <>
-      {/* <Login /> */}
-      {/* <EmployeeDashboard /> */}
-      <AdminDashboard />
+      <Login />
+      {/* <EmployeeDashboard />
+      <AdminDashboard />  */}
     </>
   )
 }
